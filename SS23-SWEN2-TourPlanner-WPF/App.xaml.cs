7af3928cb482712880009c1f09f4d5832c7cb481
Leonard Struck <me@leonardstruck.com>
2023-05-09 14:40:30 +0200

    Add project files.

---
+++ SS23-SWEN2-TourPlanner-WPF/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SS23_SWEN2_TourPlanner_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            var window = new MainWindow();
+
+            window.Show();
+        }
+    }
+}
