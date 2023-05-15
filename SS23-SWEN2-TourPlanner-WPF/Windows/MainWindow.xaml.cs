1637f1bf7cd2662891281423cd877830370fd489
Leonard Struck <me@leonardstruck.com>
2023-05-15 08:55:13 +0200

    select current Tour

--- SS23-SWEN2-TourPlanner-WPF/Windows/MainWindow.xaml.cs
+++ SS23-SWEN2-TourPlanner-WPF/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SS23_SWEN2_TourPlanner_WPF.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,11 @@ namespace SS23_SWEN2_TourPlanner_WPF.Windows
     /// </summary>
     public partial class MainWindow : Window
     {
+        internal BooleanToVisibilityConverter btvc;
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = App.Current.Services.GetService(typeof(ToursViewModel));
         }
     }
 }
