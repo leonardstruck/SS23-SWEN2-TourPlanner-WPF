d5afc2c3a97785560fac91d541b3a87d583d5f8b
Bernhard Utz <if21b241@technikum-wien.at>
2023-05-23 08:00:01 +0200

    choose filepath

--- SS23-SWEN2-TourPlanner-WPF.BL/Report.cs
+++ SS23-SWEN2-TourPlanner-WPF.BL/Report.cs
@@ -2,98 +2,106 @@
 using iText.Layout;
 using iText.Layout.Element;
 using iText.Layout.Properties;
+using Microsoft.Win32;
 using SS23_SWEN2_TourPlanner_WPF.Models;
 using System;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Windows.Input;
 //using System.Windows.Documents;
 
 namespace SS23_SWEN2_TourPlanner_WPF.BL
 {
     public class Report
     {
-
+        
         public void CreateReport(ObservableCollection<Tour> tours)
         {
-            // reports werden mal am Desktop gespeichert. Downloads wär cleaner aber hab ich nicht gefunden grad :(
-            var path = System.IO.Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "TourPlanner", "Reports");
-            System.IO.Directory.CreateDirectory(path);
-            PdfWriter writer = new PdfWriter(System.IO.Path.Join(path, ($"{DateTime.Now.ToString("yyyyMMddHHmmssfff")}.pdf")));
-            var pdf = new PdfDocument(writer);
-            var document = new Document(pdf);
-            document.Add(
+            // Show save file dialog
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF Files (*.pdf)|*.pdf";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                // Create the PDF report
+                PdfWriter writer = new PdfWriter(filePath);
+                var pdf = new PdfDocument(writer);
+                var document = new Document(pdf);
+                document.Add(
                 new Paragraph("Tour Report")
                     .SetFontSize(64)
                     .SetBold()
                     .SetFontColor(iText.Kernel.Colors.ColorConstants.DARK_GRAY)
                 );
-            document.Add(
-                new Paragraph(DateTime.Now.ToString("dd.MMM yyyy"))
-                    .SetFontColor(iText.Kernel.Colors.ColorConstants.LIGHT_GRAY)
-                );
-            foreach (Tour tour in tours)
-            {
-                document.Add(new AreaBreak(AreaBreakType.NEXT_PAGE));
-
-                //header
                 document.Add(
-                    new Paragraph(tour.Name)
-                        .SetFontSize(32)
-                        .SetBold()
-                        .SetFontColor(iText.Kernel.Colors.ColorConstants.DARK_GRAY)
-                    );
-                // details
-                document.Add(
-                    new Paragraph($"{tour.From} - {tour.To}, {tour.Distance}km {Math.Floor(tour.Time)}:{(tour.Time%1)*60}")
-                        .SetFontSize(18)
+                    new Paragraph(DateTime.Now.ToString("dd.MMM yyyy"))
                         .SetFontColor(iText.Kernel.Colors.ColorConstants.LIGHT_GRAY)
                     );
-                //description
-                document.Add(
-                    new Paragraph(tour.Description)
-                       .SetFontSize(16)
-                       .SetFontColor(iText.Kernel.Colors.ColorConstants.DARK_GRAY)
-                    );
-                // Tourlogs
-                if(tour.TourLogs.Count != 0)
+                foreach (Tour tour in tours)
                 {
+                    document.Add(new AreaBreak(AreaBreakType.NEXT_PAGE));
+
+                    //header
                     document.Add(
-                        new Paragraph("Logs")
+                        new Paragraph(tour.Name)
+                            .SetFontSize(32)
+                            .SetBold()
+                            .SetFontColor(iText.Kernel.Colors.ColorConstants.DARK_GRAY)
+                        );
+                    // details
+                    document.Add(
+                        new Paragraph($"{tour.From} - {tour.To}, {tour.Distance}km {Math.Floor(tour.Time)}:{(tour.Time % 1) * 60}")
+                            .SetFontSize(18)
+                            .SetFontColor(iText.Kernel.Colors.ColorConstants.LIGHT_GRAY)
+                        );
+                    //description
+                    document.Add(
+                        new Paragraph(tour.Description)
                            .SetFontSize(16)
-                           .SetBold()
                            .SetFontColor(iText.Kernel.Colors.ColorConstants.DARK_GRAY)
                         );
-                    Table table = new Table(5);
-                    table.AddHeaderCell("Date");
-                    table.AddHeaderCell("Comment");
-                    table.AddHeaderCell("Difficulty");
-                    table.AddHeaderCell("Total Time");
-                    table.AddHeaderCell("Rating");
-                    foreach (TourLog tourLog in tour.TourLogs)
+                    // Tourlogs
+                    if (tour.TourLogs.Count != 0)
                     {
-                        table.AddCell(tourLog.DateTime.ToString("dd.MM.yyyy"));
-                        table.AddCell(tourLog.Comment);
-                        table.AddCell(tourLog.Difficulty.ToString());
-                        table.AddCell(tourLog.TotalTime.ToString());
-                        table.AddCell(tourLog.Rating.ToString());
+                        document.Add(
+                            new Paragraph("Logs")
+                               .SetFontSize(16)
+                               .SetBold()
+                               .SetFontColor(iText.Kernel.Colors.ColorConstants.DARK_GRAY)
+                            );
+                        Table table = new Table(5);
+                        table.AddHeaderCell("Date");
+                        table.AddHeaderCell("Comment");
+                        table.AddHeaderCell("Difficulty");
+                        table.AddHeaderCell("Total Time");
+                        table.AddHeaderCell("Rating");
+                        foreach (TourLog tourLog in tour.TourLogs)
+                        {
+                            table.AddCell(tourLog.DateTime.ToString("dd.MM.yyyy"));
+                            table.AddCell(tourLog.Comment);
+                            table.AddCell(tourLog.Difficulty.ToString());
+                            table.AddCell(tourLog.TotalTime.ToString());
+                            table.AddCell(tourLog.Rating.ToString());
+                        }
+                        document.Add(table);
                     }
-                    document.Add(table);
-                }
-                // Image
-                if (File.Exists(tour.Image))
-                {
-                    document.Add(
-                        new Image(iText.IO.Image.ImageDataFactory.Create(tour.Image))
-                            .SetWidth(300)
-                            .SetHeight(300)
-                            .SetTextAlignment(TextAlignment.CENTER)
-                            .SetMarginTop(20)
-                        );
+                    // Image
+                    if (File.Exists(tour.Image))
+                    {
+                        document.Add(
+                            new Image(iText.IO.Image.ImageDataFactory.Create(tour.Image))
+                                .SetWidth(300)
+                                .SetHeight(300)
+                                .SetTextAlignment(TextAlignment.CENTER)
+                                .SetMarginTop(20)
+                            );
+                    }
+
                 }
 
+                document.Close();
             }
-            
-            document.Close();
         }
     }
 }
