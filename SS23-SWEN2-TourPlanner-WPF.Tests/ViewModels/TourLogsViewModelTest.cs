f3a241e4531be42cd76053ff9db8d3e2019ee0f8
Leonard Struck <me@leonardstruck.com>
2023-05-30 12:25:20 +0200

    add first unit tests

---
+++ SS23-SWEN2-TourPlanner-WPF.Tests/ViewModels/TourLogsViewModelTest.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using SS23_SWEN2_TourPlanner_WPF.BL;
+using SS23_SWEN2_TourPlanner_WPF.Models;
+using SS23_SWEN2_TourPlanner_WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS23_SWEN2_TourPlanner_WPF.Tests.ViewModels
+{
+    [TestFixture]
+    public class TourLogsViewModelTest
+    {
+        private Mock<IToursManager> _toursManagerMock;
+        private TourLogsViewModel _viewModel;
+        private Tour _tour;
+
+        [SetUp]
+        public void Setup()
+        {
+            _toursManagerMock = new Mock<IToursManager>();
+            _tour = new Tour("Test-Tour")
+            {
+                Id = 1,
+            };
+
+            _tour.TourLogs.Add(new TourLog() { Id = 1 });
+            _tour.TourLogs.Add(new TourLog() { Id = 2 });
+
+            _viewModel = new TourLogsViewModel(_toursManagerMock.Object, _tour);
+        }
+    }
+}
