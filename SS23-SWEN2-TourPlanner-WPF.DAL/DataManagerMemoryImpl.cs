de7d9c590c91a9d40b3bf281c57f11756f6233f9
Leonard Struck <me@leonardstruck.com>
2023-06-02 14:32:43 +0200

    ID Counter

--- SS23-SWEN2-TourPlanner-WPF.DAL/DataManagerMemoryImpl.cs
+++ SS23-SWEN2-TourPlanner-WPF.DAL/DataManagerMemoryImpl.cs
@@ -12,9 +12,11 @@ namespace SS23_SWEN2_TourPlanner_WPF.DAL
         private readonly List<Tour> tours = new();
         private readonly Dictionary<int, List<TourLog>> tourLogs = new();
 
+        private int _idCounter = 0;
+
         public Tour AddTour(Tour t)
         {
-            t.Id = tours.Count;
+            t.Id = ++_idCounter;
             tours.Add(t);
             return t;
         }
