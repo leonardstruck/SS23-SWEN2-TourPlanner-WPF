ca8a3db1c2b4d53a6e74f28d925581c0dc804e7e
Leonard Struck <me@leonardstruck.com>
2023-06-02 09:10:25 +0200

    unnecessary using

--- SS23-SWEN2-TourPlanner-WPF/Views/TourDetailsView.xaml.cs
+++ SS23-SWEN2-TourPlanner-WPF/Views/TourDetailsView.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using SS23_SWEN2_TourPlanner_WPF.ViewModels;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
