b40e9fee82b8898cdc0f36fffa9921e158610629
Bernhard Utz <if21b241@technikum-wien.at>
2023-06-03 14:10:35 +0200

    TourLog Tests

--- SS23-SWEN2-TourPlanner-WPF.Models/TourLog.cs
+++ SS23-SWEN2-TourPlanner-WPF.Models/TourLog.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 
 namespace SS23_SWEN2_TourPlanner_WPF.Models
@@ -17,7 +19,7 @@ namespace SS23_SWEN2_TourPlanner_WPF.Models
         Challenging
     }
 
-    public class TourLog
+    public class TourLog : ICloneable
     {     
         public int Id { get; set; }
         public DateTime DateTime { get; set; }
@@ -65,7 +67,64 @@ namespace SS23_SWEN2_TourPlanner_WPF.Models
                 Console.WriteLine(e.Message);
             }
         }
+        public static bool operator ==(TourLog? left, TourLog? right)
+        {
+            if (left == null && right == null)
+                return true;
+
+            if(left == null || right == null) 
+                return false;
+
+            return (
+                left.Id == right.Id &&
+                left.DateTime == right.DateTime &&
+                left.Comment == right.Comment &&
+                left.Difficulty == right.Difficulty &&
+                left.TotalTime == right.TotalTime &&
+                left.Rating == right.Rating
+            );
+        }
+
+        public static bool operator !=(TourLog? left, TourLog? right)
+        {
+            return !(left == right);
+        }
+        public override bool Equals(Object obj)
+        {
+            //Check for null and compare run-time types.
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                TourLog temp = (TourLog)obj;
+                return (
+                    this.Id == temp.Id &&
+                    this.DateTime == temp.DateTime &&
+                    this.Comment == temp.Comment &&
+                    this.Difficulty == temp.Difficulty &&
+                    this.TotalTime == temp.TotalTime &&
+                    this.Rating == temp.Rating
+                );
+            }
+        }
+
+        public object Clone()
+        {
+            return new TourLog()
+            {
+                Id = this.Id,
+                DateTime = this.DateTime,
+                Comment = this.Comment,
+                Difficulty = this.Difficulty,
+                TotalTime = this.TotalTime,
+                Rating = this.Rating
+            };
+        }
     }
 
     
+
+
 }
