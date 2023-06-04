503cb354eaf68a42afa915687a76996416f8937a
Leonard Struck <me@leonardstruck.com>
2023-06-04 13:26:14 +0200

    display distance and time

---
+++ SS23-SWEN2-TourPlanner-WPF/Converters/NullToVisibility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SS23_SWEN2_TourPlanner_WPF.Converters
+{
+    public class NullToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is TimeSpan t)
+            {
+                return t.TotalSeconds == 0 ? Visibility.Collapsed : Visibility.Visible;
+            }
+            if(value is double d)
+            {
+                return d == 0 ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
